3fb7fe8ae5f05226087f7016529e9207272a95e0
jaynarayan20 <jaynarayan540@gmail.com>
2022-10-01 13:43:38 +0530

    Added Funfact page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-import './App.css'
+// import './App.css'
 import Navbar from "./Components/Navbar";
-
+import { Routes, Route, HashRouter } from 'react-router-dom';
+import Funfacts from "./pages/Funfacts";
+// import Homepage from "./pages/Homepage";
+// import Quiz from "./pages/Quiz";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div className="font-Raleway bg-[url('./assets/shark.png')] min-h-screen">
       <Navbar />
+      <HashRouter>
+					<Routes>
+						{/* <Route path="home" element={<Homepage />} /> */}
+						<Route path="funfact" element={<Funfacts />} />
+            {/* <Route path="quiz" element={<Quiz />} /> */}
+					</Routes>
+			</HashRouter>
     </div>
   )
 }
---
+++ src/pages/Funfacts.jsx
@@ -0,0 +1,8 @@
+
+export default function  Funfacts() {
+    return(
+        <main>
+            <h1>i am a shark.</h1>
+        </main>
+    )
+}
