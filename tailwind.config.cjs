164dcfee56df3789d21cae798e0e24865c6381df
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-10-01 23:36:16 +0530

    added linear gradient for quiz card

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'main-pattern': "linear-gradient(to bottom, rgba(7, 57, 75, 0.5), rgba(9, 27, 36, 0.8)), url('./src/assets/bg-ocean.png')",
-      },
       fontFamily: {
         Berkshire: ['Berkshire Swash', 'cursive'],
         Raleway: ['Raleway', 'sans-serif'],
