8b684e7c390660050f2db7e219cd0239ced292d0
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-10-01 15:04:57 +0530

    fix: navbar padding

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
 import sharkLogo from '../assets/shark-logo.svg'
+import githubIcon from'../assets/githubIcon.svg'
 
 function Navbar() {
   return(
-    <nav className="bg-black/50 py-4 px-20 flex justify-between">
+    <nav className=" h-[12vh] px-20 flex justify-between">
       <a className="flex items-center" href="#">
         <img className="w-10" src={sharkLogo} alt="shark-logo" />
         <p className="font-Berkshire text-white text-2xl font-bold">SharkFolio</p>
       </a>
       <div className="flex items-center justify-between gap-8">
         <a href="/#/funfact" className="text-lg text-white">Fun Facts</a>
-        <a href="#" className="text-lg text-white">Quiz</a>
+        <a href="/#/quiz" className="text-lg text-white">Quiz</a>
+        <a href="https://github.com/RakeshSangem/Sharkfolio" ><img className="w-6" src={githubIcon} alt="" /></a>
       </div>
+
     </nav>
   )
 }
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,9 @@
+export default function Quiz() {
+   return(
+      <main className="w-full min-h-[90vh] ">
+         <div className="w-50wh flex items-center">
+            <h3>Quiz Time</h3>
+         </div>
+      </main>
+   )
+}
