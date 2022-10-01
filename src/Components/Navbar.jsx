76bd7d8fd9af45a27420e5af73328aa2ca2d4cda
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-10-01 23:04:28 +0530

    add hover effects to answer buttons and links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,16 +4,15 @@ import githubIcon from'../assets/githubIcon.svg'
 function Navbar() {
   return(
     <nav className="w-full h-[12vh] px-20 flex justify-between absolute top-0">
-      <a className="flex items-center" href="#">
-        <img className="w-10" src={sharkLogo} alt="shark-logo" />
-        <p className="font-Berkshire text-white text-2xl font-bold">SharkFolio</p>
+      <a className="flex items-center hover:scale-105 transform transition duration-300" href="#">
+        <img className="w-8 mr-1" src={sharkLogo} alt="shark-logo" />
+        <p className="font-Berkshire text-white tracking-wider text-2xl">SharkFolio</p>
       </a>
-      <div className="flex items-center justify-between gap-8">
-        <a href="/#/funfact" className="text-lg text-white">Fun Facts</a>
-        <a href="/#/quiz" className="text-lg text-white">Quiz</a>
-        <a href="https://github.com/RakeshSangem/Sharkfolio" ><img className="w-6" src={githubIcon} alt="" /></a>
+      <div className="flex items-center justify-between gap-8 font-bold">
+        <a href="/#/funfact" className="text-lg text-white hover:scale-105 transform transition duration-300 hover:opacity-50">Fun Facts</a>
+        <a href="/#/quiz" className="text-lg text-white hover:scale-105 transform transition duration-300 hover:opacity-50">Quiz</a>
+        <a className="hover:scale-105 transform transition duration-300 hover:opacity-50" href="https://github.com/RakeshSangem/Sharkfolio" ><img className="w-6" src={githubIcon} alt="" /></a>
       </div>
-
     </nav>
   )
 }
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -8,10 +8,10 @@ export default function Quiz() {
             <h3 className="p-4 text-4xl text-[#CDEFFF] border-[#6EE9FF] border-b-2 w-full font-bold text-center">Quiz Time</h3>
             <h2 className="text-3xl text-[#70D9FF] py-16 font-semibold tracking-wide">Does shark eat biryani or pineapple pizza? </h2>
             <div className="grid grid-cols-2 gap-x-10 px-10 gap-y-10 w-full pb-10">
-               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold">Biryani eats</button>
-               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
-               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
-               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold transform transition duration-500 hover:scale-105 hover:bg-[#6EE9FF] hover:text-black">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold transform transition duration-500 hover:scale-105 hover:bg-[#6EE9FF] hover:text-black">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold transform transition duration-500 hover:scale-105 hover:bg-[#6EE9FF] hover:text-black">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold transform transition duration-500 hover:scale-105 hover:bg-[#6EE9FF] hover:text-black">Biryani eats</button>
             </div>
             <div className="text-xl rounded-[15px] px-3 py-1  text-[#6EE9FF]  mb-10">
                <p id="p-number">1<span>/10</span></p>
