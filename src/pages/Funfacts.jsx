e980ed93a5140f916a2fbc7ef7fa6ad1a42fd621
jaynarayan20 <jaynarayan540@gmail.com>
2022-10-01 13:51:23 +0530

    add info

--- src/pages/Funfacts.jsx
+++ src/pages/Funfacts.jsx
@@ -2,7 +2,7 @@
 export default function  Funfacts() {
     return(
         <main>
-            <h1>i am a shark.</h1>
+            <h1>i am a shark. this is shark world</h1>
         </main>
     )
 }
