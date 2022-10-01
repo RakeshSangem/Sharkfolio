6d86665ada3de5ecf7a90cf43d4a9531900bb2ce
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-10-01 17:08:24 +0530

    added quiz and fixed ui issues

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,8 +1,21 @@
+import Navbar from "../Components/Navbar";
+
 export default function Quiz() {
    return(
-      <main className="w-full min-h-[90vh] ">
-         <div className="w-50wh flex items-center">
-            <h3>Quiz Time</h3>
+      <main className="w-full flex bg-main-pattern items-center justify-center min-h-screen text-white relative ">
+         <Navbar />
+         <div className="flex flex-col h-full w-[60vw] rounded-3xl items-center justify-between border-[#6EE9FF] border-2 bg-[#064550]/40 mt-8">
+            <h3 className="p-4 text-4xl text-[#CDEFFF] border-[#6EE9FF] border-b-2 w-full font-bold text-center">Quiz Time</h3>
+            <h2 className="text-3xl text-[#70D9FF] py-16 font-semibold tracking-wide">Does shark eat biryani or pineapple pizza? </h2>
+            <div className="grid grid-cols-2 gap-x-10 px-10 gap-y-10 w-full pb-10">
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF] font-semibold">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
+               <button className="border-[#6EE9FF] border-2 rounded-full p-2 text-[#70D9FF]  font-semibold">Biryani eats</button>
+            </div>
+            <div className="text-xl rounded-[15px] px-3 py-1  text-[#6EE9FF]  mb-10">
+               <p id="p-number">1<span>/10</span></p>
+            </div>
          </div>
       </main>
    )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'main-pattern': "linear-gradient(to bottom, rgba(7, 57, 75, 0.5), rgba(9, 27, 36, 0.8)), url('./src/assets/bg-ocean.png')",
+      },
       fontFamily: {
         Berkshire: ['Berkshire Swash', 'cursive'],
         Raleway: ['Raleway', 'sans-serif'],
