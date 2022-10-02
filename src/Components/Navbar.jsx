2a27d6ec1d9478b2de8faf48057fe431e06bdf46
zelfroster <sumitkumarsoni123@gmail.com>
2022-10-02 10:50:37 +0530

    feat: Fixed UI elements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,12 +6,12 @@ function Navbar() {
     <nav className="w-full h-[12vh] px-20 flex justify-between absolute top-0">
       <a className="flex items-center hover:scale-105 transform transition duration-300" href="#">
         <img className="w-8 mr-1" src={sharkLogo} alt="shark-logo" />
-        <p className="font-Berkshire text-white tracking-wider text-2xl">SharkFolio</p>
+        <p className="font-Berkshire text-white tracking-wide text-2xl">SharkFolio</p>
       </a>
-      <div className="flex items-center justify-between gap-8 font-bold">
-        <a href="/#/funfact" className="text-lg text-white hover:scale-105 transform transition duration-300 hover:opacity-50">Fun Facts</a>
-        <a href="/#/quiz" className="text-lg text-white hover:scale-105 transform transition duration-300 hover:opacity-50">Quiz</a>
-        <a className="hover:scale-105 transform transition duration-300 hover:opacity-50" href="https://github.com/RakeshSangem/Sharkfolio" ><img className="w-6" src={githubIcon} alt="" /></a>
+      <div className="flex items-center justify-between gap-8 font-semibold">
+        <a href="/#/funfact" className="text-shadow text-lg text-white transition duration-200 hover:text-[#6EE9FF]">Fun Facts</a>
+        <a href="/#/quiz" className="text-shadow text-lg text-white transition duration-300 hover:text-[#6EE9FF]">Quiz</a>
+        <a className="hover:scale-105 transform transition duration-300" href="https://github.com/RakeshSangem/Sharkfolio" ><img className="w-8 github-logo" src={githubIcon} alt="" /></a>
       </div>
     </nav>
   )
--- src/pages/Funfacts.jsx
+++ src/pages/Funfacts.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Components/Navbar";
 
 export default function Funfacts() {
   return (
-    <main className="w-full flex bg-main-pattern items-center justify-center min-h-screen text-white relative ">
+    <main className="w-full flex bg-normal bg-no-repeat bg-cover items-center justify-center min-h-screen text-white relative ">
       <Navbar />
       <h1>i am a shark. this is shark world</h1>
     </main>
