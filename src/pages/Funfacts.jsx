1196280e8026881dcaabfb682f87939669b85ee4
jaynarayan20 <jaynarayan540@gmail.com>
2022-10-02 11:27:21 +0530

    fixed file paths

--- src/pages/Funfacts.jsx
+++ src/pages/Funfacts.jsx
@@ -1,4 +1,6 @@
 import Navbar from "../Components/Navbar";
+import sharkfact from "../assets/sharkfact.png";
+import shuffle from "../assets/shuffle.svg";
 
 export default function Funfacts() {
   return (
@@ -7,7 +9,7 @@ export default function Funfacts() {
       <section className="w-[80vw] flex flex-col items-center p-16 gap-16">
                 <div className="flex w-full justify-between gap-16">
                     <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
-                        <img src="./src/assets/sharkfact.png" alt="" />
+                        <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[30vw] flex-col justify-self-end items-center justify-center text-center">
                         <h1 className="font-bold text-[40px] text-white" >sharks eats biryani sdkfds;khfk</h1>
@@ -15,7 +17,7 @@ export default function Funfacts() {
                     </div>
                 </div>
                 <button className="flex items-center tracking-wide gap-2 rounded-lg px-5 py-3 bg-[#585BAC] text-white font-semibold">
-                    <img className="w-10" src="./src/assets/shuffle.svg" alt=""/> Shuffle</button> 
+                    <img className="w-10" src={shuffle} alt=""/> Shuffle</button> 
             </section>
     </main>
   );
