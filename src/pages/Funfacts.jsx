aabb6b00ceb4b4e7090a0ec390d82426f9497915
jaynarayan20 <jaynarayan540@gmail.com>
2022-10-02 11:13:56 +0530

    feat: added Funfact page

--- src/pages/Funfacts.jsx
+++ src/pages/Funfacts.jsx
@@ -4,7 +4,19 @@ export default function Funfacts() {
   return (
     <main className="w-full flex bg-normal bg-no-repeat bg-cover items-center justify-center min-h-screen text-white relative ">
       <Navbar />
-      <h1>i am a shark. this is shark world</h1>
+      <section className="w-[80vw] flex flex-col items-center p-16 gap-16">
+                <div className="flex w-full justify-between gap-16">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
+                        <img src="./src/assets/sharkfact.png" alt="" />
+                    </div>
+                    <div className="flex w-[30vw] flex-col justify-self-end items-center justify-center text-center">
+                        <h1 className="font-bold text-[40px] text-white" >sharks eats biryani sdkfds;khfk</h1>
+                        <a href="#" className="underline text-white" >Know More</a>
+                    </div>
+                </div>
+                <button className="flex items-center tracking-wide gap-2 rounded-lg px-5 py-3 bg-[#585BAC] text-white font-semibold">
+                    <img className="w-10" src="./src/assets/shuffle.svg" alt=""/> Shuffle</button> 
+            </section>
     </main>
   );
 }
