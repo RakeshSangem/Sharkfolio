040f2504b691e64ee6405f5b72c03b35a8164a54
Sumit Kumar Soni <93514460+zelfroster@users.noreply.github.com>
2022-10-02 10:43:15 +0530

    feat: Created Home Page and Added Animations (#23)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // import { useState } from 'react'
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import Funfacts from "./pages/Funfacts";
-// import Homepage from "./pages/Homepage";
+import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
+import SharksInfo from "./pages/SharksInfo";
 function App() {
   // const [count, setCount] = useState(0)
   return (
     <div className="font-Raleway min-h-screen">
       <HashRouter>
 					<Routes>
-						{/* <Route path="home" element={<Homepage />} /> */}
+						<Route path="/" element={<Home />} />
 						<Route path="funfact" element={<Funfacts />} />
             <Route path="quiz" element={<Quiz />} />
+            <Route path="sharksinfo" element={<SharksInfo />} />
 					</Routes>
 			</HashRouter>
     </div>
---
+++ src/pages/SharksInfo.jsx
@@ -0,0 +1,78 @@
+import Navbar from "../Components/Navbar";
+
+export default function SharksInfo() {
+  return (
+    <main className="w-full flex bg-normal bg-no-repeat bg-cover bg-fixed justify-center min-h-screen text-white relative ">
+      <Navbar />
+      <section className="w-[80vw] mt-40">
+        <div className="flex flex-col items-center justify-between gap-12">
+          <div className="text-center drop-shadow-lg">
+            <h3 className="text-white text-6xl font-semibold">Sharks Info</h3>
+          </div>
+          <div className="grid grid-cols-3 gap-20">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+              <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
+              <p className="text-sm text-black p-2 leading-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
