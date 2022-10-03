dbb8dd4585d2e104080b4f12d53ddf50a867e818
Ali Reza <99729607+AliReza1083@users.noreply.github.com>
2022-10-03 13:35:20 +0800

    feat: added animation (#36)
    * I've added animation
    
    * Update yarn.lock
    
    * Delete package-lock.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,37 @@
 import Navbar from "../Components/Navbar";
 
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+
 export default function Home() {
+
+  const welcome = useRef(null);
+  const shark = useRef(null);
+  const paragraph = useRef(null);
+
+  useEffect(() => {
+    gsap.to(welcome.current, {
+      x: 0,
+      color: "white",
+      opacity: 1,
+      duration: 1
+    })
+
+    gsap.to(shark.current, {
+      x: 0,
+      opacity: 1,
+      delay: .2,
+      duration: 1
+    })
+
+    gsap.to(paragraph.current, {
+      x: 0,
+      opacity: 1,
+      delay: .5,
+      duration: 1
+    })
+  }, [])
+
   return (
     <main className="w-full flex items-center justify-center min-h-screen text-white relative overflow-hidden">
       <div className="wave-effect absolute w-full h-full bg-no-repeat bg-main bg-cover -z-10"></div>
@@ -8,10 +39,10 @@ export default function Home() {
       <section className="w-[80vw] h-full">
         <div className="flex flex-col items-center justify-between min-h-[42vh]">
           <div className="text-center drop-shadow-lg">
-            <h3 className="text-white text-6xl font-semibold">Welcome to</h3>
-            <h1 className="bg-gradient-to-b from-[#8DFBFF] to-[#308EC7] bg-clip-text text-8xl font-extrabold text-transparent">Shark World</h1>
+            <h3 className="text-6xl font-semibold opacity-0 -translate-x-20" ref={welcome}>Welcome to</h3>
+            <h1 className="bg-gradient-to-b from-[#8DFBFF] to-[#308EC7] bg-clip-text text-8xl font-extrabold text-transparent -translate-x-20 opacity-0" ref={shark}>Shark World</h1>
           </div>
-          <p className="text-lg font-light text-center w-1/2">
+          <p className="text-lg font-light text-center w-1/2 opacity-0 -translate-x-20" ref={paragraph}>
             We welcome you to the fun and amazing world of Sharks.
             <br />Here you can learn about different type of sharks and also play a mini quiz game.
           </p>
