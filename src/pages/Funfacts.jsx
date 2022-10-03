7963bd1c3635e6dfee96a6a4dafa430d9485dd10
Ali Reza <webdeve1083@gmail.com>
2022-10-03 10:44:33 +0800

    I've added animation

--- src/pages/Funfacts.jsx
+++ src/pages/Funfacts.jsx
@@ -1,127 +1,165 @@
 import Navbar from "../Components/Navbar";
 import sharkfact from "../assets/sharkfact.png";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef, useEffect } from "react";
+
 
 export default function Funfacts() {
+
+    const img = useRef(null)
+    const facts = useRef(null)
+    const link = useRef(null)
+
+    const main = useRef(null)
+    const q = gsap.utils.selector(main);
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    useEffect(() => {
+        gsap.to(q('#img'), {
+            y: 0,
+            opacity: 1,
+            stagger: .1,
+            scale: 1,
+            duration: 1
+        })
+
+        gsap.to(q('.facts'), {
+            x: 0,
+            opacity: 1,
+            delay: .3,
+            duration: 1
+        })
+
+        gsap.to(q('.links'), {
+            x: 0,
+            opacity: 1,
+            delay: .4,
+            duration: 1
+        })
+    }, [])
+
     return (
-        <main className="w-full flex-col flex bg-fixed bg-normal bg-no-repeat bg-cover items-center justify-center min-h-screen text-white relative ">
+        <main className="w-full flex-col flex bg-fixed bg-normal bg-no-repeat bg-cover items-center justify-center min-h-screen text-white relative overflow-hidden" ref={main}>
             <Navbar />
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14 mt-32">
                 <div className="flex w-full justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg] scale-150 translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Sharks do not have bones.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24">Sharks do not have bones.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Most sharks have good eyesight.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Most sharks have good eyesight.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Sharks have special electroreceptor organs.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Sharks have special electroreceptor organs.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" > Shark skin feels similar to sandpaper.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" > Shark skin feels similar to sandpaper.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Sharks can go into a trance. </h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Sharks can go into a trance. </h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Sharks have special electroreceptor organs.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Sharks have special electroreceptor organs.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
-                <div className="flex w-full justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
+                <div className="flex w-full justify-between gap-16 mt-8 facts opacity-0 -translate-x-24">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Sharks have been around a very long time.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
-                    </div>
-                </div>
-            </section>
-
-            <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
-                <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg]">
-                        <img src={sharkfact} alt="" />
-                    </div>
-                    <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Scientists age sharks by counting the rings on their vertebrae.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
-                    </div>
-                </div>
-            </section>
-
-            <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
-                <div className="flex w-full justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg]">
-                        <img src={sharkfact} alt="" />
-                    </div>
-                    <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Blue sharks are really blue.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Sharks have been around a very long time.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
 
             <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
                 <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
-                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg]">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg] translate-y-24 opacity-0" id="img">
                         <img src={sharkfact} alt="" />
                     </div>
                     <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
-                        <h1 className="font-bold text-[40px] text-white" >Each whale shark’s spot pattern is unique as a fingerprint.</h1>
-                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white" >Know More</a>
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Scientists age sharks by counting the rings on their vertebrae.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
+                <div className="flex w-full justify-between gap-16 mt-8 facts opacity-0 -translate-x-24">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white -rotate-[25deg] translate-y-24 opacity-0" id="img">
+                        <img src={sharkfact} alt="" />
+                    </div>
+                    <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Blue sharks are really blue.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-[75vw] flex flex-col items-center p-16 gap-14">
+                <div className="flex w-full flex-row-reverse justify-between gap-16 mt-8">
+                    <div className="w-[20vw] shadow-black shadow-xl bg-white rotate-[25deg] translate-y-24 opacity-0" id="img">
+                        <img src={sharkfact} alt="" />
+                    </div>
+                    <div className="flex w-[35vw] flex-col justify-self-end items-center justify-center text-center">
+                        <h1 className="font-bold text-[40px] text-white facts opacity-0 -translate-x-24" >Each whale shark’s spot pattern is unique as a fingerprint.</h1>
+                        <a href="https://www.fisheries.noaa.gov/feature-story/12-shark-facts-may-surprise-you" className="underline text-white links opacity-0 -translate-x-24" >Know More</a>
                     </div>
                 </div>
             </section>
--- src/pages/SharksInfo.jsx
+++ src/pages/SharksInfo.jsx
@@ -1,6 +1,22 @@
 import Navbar from "../Components/Navbar";
 
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+
 export default function SharksInfo() {
+
+  const cardInfo = useRef(null);
+  const q2 = gsap.utils.selector(cardInfo);
+
+  useEffect(() => {
+    gsap.to(q2('.card'), {
+      y: 0,
+      opacity: 1,
+      stagger: .2,
+      duration: .5
+    })
+  }, [])
+
   return (
     <main className="w-full flex bg-normal bg-no-repeat bg-cover bg-fixed justify-center min-h-screen text-white relative ">
       <Navbar />
@@ -9,62 +25,62 @@ export default function SharksInfo() {
           <div className="text-center drop-shadow-lg">
             <h3 className="text-white text-6xl font-semibold">Sharks Info</h3>
           </div>
-          <div className="grid grid-cols-3 gap-20">
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+          <div className="grid grid-cols-3 gap-20" ref={cardInfo}>
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
               </p>
             </div>
-            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md">
+            <div className="flex flex-col w-full bg-[#ffffffcc] items-center justify-center p-2 rounded-md card -translate-y-24 opacity-0">
               <div className="w-full h-56 bg-cyan-700 rounded-md"></div>
               <p className="text-sm text-black p-2 leading-4">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, repudiandae.
