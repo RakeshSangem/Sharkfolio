cf9676c180572cfe90c2019ef11d7ce21892dc8c
Rakesh Sangem <107752425+RakeshSangem@users.noreply.github.com>
2022-12-14 14:03:35 +0530

    Fix: homepage resonsiveness (#49)
    * feat: add responsiveness to navbar
    
    * add responsiveness to homepage
    
    * fix: paragraph width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Navbar from "../Components/Navbar";
-
 import { gsap } from "gsap";
 import { useRef, useEffect } from "react";
 
@@ -35,24 +34,24 @@ const Home = () => {
     <main className="w-full flex items-center justify-center min-h-screen text-white relative overflow-hidden">
       <div className="wave-effect absolute w-full h-full bg-no-repeat bg-main bg-cover -z-10"></div>
       <Navbar />
-      <section className="w-[80vw] h-full">
-        <div className="flex flex-col items-center justify-between min-h-[42vh]">
+      <section className="w-[80vw] h-full md:w-[90vw]">
+        <div className="flex flex-col items-center justify-between">
           <div className="text-center drop-shadow-lg">
             <h3
-              className="text-5xl font-semibold opacity-0 -translate-x-20"
+              className="text-5xl font-semibold opacity-0 -translate-x-20 lg:text-5xl sm:text-2xl"
               ref={welcome}
             >
               Welcome to
             </h3>
             <h1
-              className="bg-gradient-to-b from-[#8DFBFF] to-[#308EC7] bg-clip-text text-8xl font-bold text-transparent -translate-x-20 opacity-0"
+              className="bg-gradient-to-b from-[#8DFBFF] to-[#308EC7] bg-clip-text text-8xl font-bold text-transparent -translate-x-20 opacity-0 sm:text-6xl md:8xl md:bg-red-400"
               ref={shark}
             >
               Shark World
             </h1>
           </div>
           <p
-            className="text-md text-teal-200/50 font-light text-center w-1/2 opacity-0 -translate-x-20"
+            className="text-md text-teal-200/50 font-light text-center w-1/2 opacity-0 -translate-x-20 sm:w-3/4 md:w-3/4 lg:w-3/4 "
             ref={paragraph}
           >
             We welcome you to the fun and amazing world of Sharks. Here you can
@@ -60,7 +59,7 @@ const Home = () => {
           </p>
           <a
             href="/#/sharksinfo"
-            className="bg-[#1F78A1] font-semibold px-24 py-2 rounded-md border-[1px] mt-8 hover:bg-[#165370] duration-300 shake-on-hover"
+            className="bg-[#1F78A1]/20 text-white/80 font-normal tracking-wide px-24 py-2 rounded-md border-[1px] border-[#165370] mt-8 hover:bg-[#165370] duration-300 shake-on-hover"
           >
             Explore
           </a>
